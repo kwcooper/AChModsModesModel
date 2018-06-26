cc686e804b2cdebc189c9f1ab46b6d8e26f04400
Keiland <officialkwade@gmail.com>
2018-06-26 17:40:26 -0400

    ud

--- thetaModMode.m
+++ thetaModMode.m
@@ -13,7 +13,12 @@ ACA3 = [0 1 1];
 
 
 % init params
-t = ; 
+%t = 1; 
+
+dt = 0.005; % 5 ms
+thF = 8; % 8 Hz
+stepsPerCycle = ceil(((1/thF)/dt));
+
 
 % Encoding   (pi/2):  PLTP = 1;  PEC = 1; PCA3 = 0;
 % Retrieval (3*pi/2): PLTP = -1; PEC = 0; PCA3 = 1;
@@ -21,25 +26,58 @@ PLTP = 1; % oscilates between 1 @ pi/2 & -1 @ 3*pi/2
 PEC  = 1; % oscilates between 1 @ pi/2 &  0 @ 3*pi/2
 PCA3 = 0; % oscilates between 1 @ pi/2 &  0 @ 3*pi/2
 
+step = .01;
 
+t = 1;
 
 % define theta functions
-thEC = .5 * sin(t + PEC) + .5;
-thCA3 = .5 * sin(t + PCA3) + .5;
-thLTP = sin(t + PLTP);
+thEC(1) = .5 * sin(t + PEC) + .5;
+thCA3(1) = .5 * sin(t + PCA3) + .5;
+thLTP(1) = sin(t + PLTP);
+
+ai(:,:,1) = [0, 0];
+% run sim
+for t = 2:stepsPerCycle
+  
+thEC(t,:) = .5 * sin(t + PEC) + .5;
+thCA3(t,:) = .5 * sin(t + PCA3) + .5;
+thLTP(t,:) = sin(t + PLTP);  
 
 % activation rule
-ai = thEC * AEC' + thCA3 * W * ACA3';
+ai(:,:,t) = thEC(t-1) * AEC' + thCA3(t-1) * W * ACA3';
 
 % learning
+%W_n = W + ;
 
-W_n = W +  
+end
 
-
-
-
-
-
+aiR = reshape(ai, size(ai,2), size(ai,3));
+figure; plot(aiR'); %plot(ai(2,:));
 
 
+%% test phasic 
+
+if 0
+  % Encoding (Extrinsic)
+  PLTP = 1; PEC = 1; PCA3 = 0;
+  for t = 2:stepsPerCycle
+    thLTP(t,:) = sin(t + PLTP);
+    thEC(t,:) = .5 * sin(t + PEC) + .5;
+    thCA3(t,:) = .5 * sin(t + PCA3) + .5;
+  end
+  
+  % Retrieval (Intrinsic)
+  PLTP = -1; PEC = 0; PCA3 = 1;
+  for t = 2:stepsPerCycle
+    thLTPR(t,:) = sin(t + PLTP);
+    thECR(t,:) = .5 * sin(t + PEC) + .5;
+    thCA3R(t,:) = .5 * sin(t + PCA3) + .5;
+  end
+  
+  figure; suptitle(' ');
+  subplot(3,1,1); hold on; plot(thLTP);  plot(thLTPR); title('LTP');
+  subplot(3,1,2); hold on; plot(thEC);   plot(thECR);  title('EC');
+  subplot(3,1,3); hold on; plot(thCA3);  plot(thCA3R); title('CA3');
+  legend;
+end
 
