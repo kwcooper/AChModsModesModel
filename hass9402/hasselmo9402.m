6c3939783ce31d820f6445a89512d85fcd425487
Keiland <officialkwade@gmail.com>
2018-07-23 19:22:28 -0400

    ud

--- hass9402/hasselmo9402.m
+++ hass9402/hasselmo9402.m
@@ -11,7 +11,7 @@ clear all;
 dbstop if error;
 
 %% runtime parameters
-p.nTrls = 2;
+p.nTrls = 1;
 p.nTSteps = 25;
 p.nCA1cells = 2;
 p.nCA3cells = 3;
@@ -23,7 +23,7 @@ p.nCycles = 6; % How many cycles would we like per trial. Default = 6?
 p.stepsPerCycle = ceil(((1/p.thF)/p.dt));
 p.phaseStep = (2*pi)/p.stepsPerCycle; % number of radians to increment theta phase by for each time steps
 p.k = .5; % Max synaptic growth. Default = .5
-p.lrate = 0.01; % learning rate, per Ehren's experiment
+p.lrate = 0.5; % learning rate, per Ehren's experiment
 p.thetaScale = 1; % X from Hasselmo et al. 2002, p 799
 % [0 to 1] smaller means theta has less effect on functional strength
 % paper is vague about whether different values are used for the different
@@ -52,56 +52,59 @@ tempXprod = nan(p.nCA1cells,p.nCA3cells,p.stepsPerCycle);
 %a.CA3(:,1) = rand(p.nCA3cells,1) > .5;
 %a.EC(:,1)  = rand(p.nECcells, 1) > .5;
 
-a.CA3(:,1) = [1; 1; 0]; 
-a.EC(:,1)  = [1; 0]; 
+a.CA3(:,1) = [0;1;1]; 
+a.EC(:,1)  = [0;1]; 
 
-a.CA3(:,2) = [0; 1; 1]; 
-a.EC(:,2)  = [0; 1]; 
+%a.CA3(:,2) = [0; 1; 1]; 
+%a.EC(:,2)  = [0; 1]; 
 
-a.CA3(:,3) = [0; 0; 1;]; 
+%a.CA3(:,3) = [0; 0; 1;]; 
 
 % TD simply setting these to 1,2,3 won't work, should allocate them to
 % spaced parts at steps of p.nCycles*p.stepsPerCycle?
 %%
+
 stage = 1; 
 % for each trial, run the theta model 
 for trl = 1:p.nTrls
   % initialize first timestep
   fprintf('\nTrial %i\n',trl);
-  
   stage = trl; % TD: Fix this
-  % run the model
-  [a, tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage);
   
+  % run the model feedforward
+  [a, tempXprod, pha, syn, theta] = runTheta(a,w,tempXprod,p,stage);
+  
+  % Learning function, update weights
   % compute weight updates after each theta cycle
   dw.CA3(:,:) = sum(tempXprod,3);
-  
-  
+
   if any(isnan(dw.CA3)), keyboard, end % Just in case something goes wrong...
-  w.CA3, dw.CA3
-  w.CA3 = w.CA3 + p.lrate .* dw.CA3; %   w.CA3 = w.CA3 + dw.CA3;
+  w.CA3 = w.CA3 + (p.lrate .* dw.CA3); %   w.CA3 = w.CA3 + dw.CA3;
   w.CA3 = min(w.CA3, p.k);
-  %w.CA3
   
 end
 
 % TD: make plot parameter
-if 1, plotStateVariables(theta,a,pha); end
+if 1, plotStateVariables(theta,a,pha,syn); end
 
-%keyboard
+keyboard
+
+a.CA3(:,1) = [1; 0; 0]; 
+a.EC(:,1)  = [0; 0]; 
 
 fprintf('Test Phase\n');
-trl = 3;
-stage = trl; 
-[a, tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage);
-if 1, plotStateVariables(theta,a,pha); end
+%trl = 3;
+%stage = trl; 
+[a, tempXprod, pha, syn, theta] = runTheta(a,w,tempXprod,p,stage);
+if 1, plotStateVariables(theta,a,pha,syn); end
+
 
 
 
 keyboard;
 end
 
-function [a,tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage)
+function [a,tempXprod, pha, syn, theta] = runTheta(a,w,tempXprod,p,stage)
   
   % allocate phase and theta params
   pha.EC = nan(p.nTSteps,1);   theta.EC = nan(p.nTSteps,1);
@@ -118,10 +121,10 @@ function [a,tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage)
   %p.nCycles = 10;
   pha.AChLvls = linspace(1,0,p.nCycles*p.stepsPerCycle); % add acetylcholine
   Cr = 1; Cl = 1; % These need to be updated to control for individual dynamics in CA3 vs EC 
-  syn.EC3(:,1) = (1 - pha.AChLvls(1)*Cl) * ((theta.EC(1) .* w.EC) * a.EC(:,stage));
-  syn.CA3(:,1) = (1 - pha.AChLvls(1)*Cr) * ((theta.CA3(1) .* w.CA3) * a.CA3(:,stage));
+  syn.EC(:,1)  = (pha.AChLvls(1) * Cl) * ((theta.EC(1) .* w.EC) * a.EC(:,stage));
+  syn.CA3(:,1) = (1 - pha.AChLvls(1) * Cr) * ((theta.CA3(1) .* w.CA3) * a.CA3(:,stage));
   
-  a.CA1(:,1) = syn.EC3(:,1) + syn.CA3(:,1); % eq 2.4 p.799
+  a.CA1(:,1) = syn.EC(:,1) + syn.CA3(:,1); % eq 2.4 p.799
 
   tempXprod(:,:,1) = (theta.LTP(1) .* a.CA1(:,1)) * a.CA3(:,stage)';
   % run one theta cycle
@@ -135,16 +138,17 @@ function [a,tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage)
     
     %a.CA1 = w.EC .* a.EC + w.CA3 .* a.CA3; % eq 2.1
     %a.CA1(:,t) = ((theta.EC(t) .* w.EC) * a.EC(:,stage)) + ((theta.CA3(t) .* w.CA3) * a.CA3(:,stage)); % eq 2.4 p.799
-    syn.EC3(:,t) = (pha.AChLvls(t)*Cl) * ((theta.EC(t) .* w.EC) * a.EC(:,stage));
+    % todo: Which ach story do we believe? Inhib EC or CA3...?
+    syn.EC(:,t) = (pha.AChLvls(t)*Cl) * ((theta.EC(t) .* w.EC) * a.EC(:,stage));
     syn.CA3(:,t) = (1 - pha.AChLvls(t)*Cr) * ((theta.CA3(t) .* w.CA3) * a.CA3(:,stage));
     
-    a.CA1(:,t) = syn.EC3(:,t) + syn.CA3(:,t); % eq 2.4 p.799
+    a.CA1(:,t) = syn.EC(:,t) + syn.CA3(:,t); % eq 2.4 p.799
 
     tempXprod(:,:,t) = (theta.LTP(t) .* a.CA1(:,t)) * a.CA3(:,stage)';
     
   end
   
-  modEC = reshape(syn.EC3,2,[])';
+  modEC = reshape(syn.EC,2,[])';
   modCA3 = reshape(syn.CA3,2,[])';
   
   if 0
@@ -163,25 +167,33 @@ function [a,tempXprod, pha, theta] = runTheta(a,w,tempXprod,p,stage)
   
 end
 
-function plotStateVariables(theta,a,pha)
+function plotStateVariables(theta,a,pha,syn)
     figure;
-    subplot(4,1,1);
+    subplot(6,1,1);
     hold off; plot(theta.EC)
     hold on; plot(theta.CA3)
     ylabel('theta');
     legend('EC','CA3'); ylim([0 1]);
     title(['End of training']);
     
-    subplot(4,1,2);
+    subplot(6,1,2);
     plot(a.CA1');
-    ylabel('Act CA1');
+    ylabel('CA1');
     
-    subplot(4,1,3);
-    plot(theta.LTP)
+    subplot(6,1,3);
+    plot(syn.EC');
+    ylabel('EC');
+    
+    subplot(6,1,4);
+    plot(syn.CA3');
+    ylabel('CA3');
+    
+    subplot(6,1,5);
+    plot(theta.LTP);
     ylabel('theta LTP');
     
-    subplot(4,1,4);
-    plot(pha.AChLvls)
+    subplot(6,1,6);
+    plot(pha.AChLvls);
     ylabel('ACh');
 end
 
