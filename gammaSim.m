c4f93ea260c06def9cb2c0dbe3af4215cf453582
Keiland <officialkwade@gmail.com>
2019-04-18 18:51:38 -0400

    ud

--- gammaSim.m
+++ gammaSim.m
@@ -4,7 +4,7 @@ function gammaSim()
 
 fs = 600;              % Sampling frequency (samples per second)
 dt = 1/fs;             % seconds per sample
-StopTime = 1;          % seconds
+StopTime = 200;        % seconds
 t = (0:dt:StopTime)';  % seconds
 
 
@@ -19,6 +19,8 @@ F = 80; % Hz
 fGamma = sin(2*pi*F*t);
 
 gScale = .3;
+
+signal = theta + gScale * (fGamma .* relu(theta)) + gScale * (sGamma .* relu(sgTheta));
 
 %%
 figure;
@@ -37,10 +39,22 @@ ylabel('Fast \gamma (EC)','fontsize',11)
 
 subplot(numPlts,1,4)
 %plot(theta + gScale * (fGamma .* relu(theta)) + gScale * (sGamma .* relu(-theta)))
-plot(t,theta + gScale * (fGamma .* relu(theta)) + gScale * (sGamma .* relu(sgTheta)))
+plot(t,signal)
 ylabel('CA1 LFP','fontsize',11)
 xlabel('Time (S)','fontsize',11)
 
+
+
+%% Generate theta mod gamma figure
+
+[modindex, thetarange, gammarange, powPhsDists, bincenters,thetaamps_M,gammaamps_M,stdVals]  = thetaModGamma_nonCMB(signal, fs,'filtParams',2,'stdGamma',1,'gammarange',[10:2:140],'thetarange',8);
+figure; imagesc(linspace(10,360,36), gammarange,powPhsDists)
+colormap 'jet';
+title('Simulated theta mod gamma')
+ylabel('Frequency (Hz)'); xlabel('\theta Phase')
+%h = colorbar; set(get(h,'label'),'string','Power (\mew V^{2})'); 
+h = colorbar; ylabel(h, 'Power (\muV^{2})','rotation', 270, 'position', [2.7, 1.6, 0], 'fontsize',11);
+axis xy;
 end
 
 function y = relu(x)
